f88adf09b72a4b9540f42d2199b6a47d9e223064
Vishal Sharma <122391309+Vishal182573@users.noreply.github.com>
2024-11-13 14:31:59 +0530

    Update constant.ts

--- frontend/src/lib/constant.ts
+++ frontend/src/lib/constant.ts
@@ -1,2 +1,3 @@
 // export const BACKEND_URL = "http://localhost:3002"
-export const BACKEND_URL = "https://kc-global-school.onrender.com"
+// export const BACKEND_URL = "https://kc-global-school-1.onrender.com"
+export const BACKEND_URL= "https://kc-global-school-production.up.railway.app"
